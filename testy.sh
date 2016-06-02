d981a5ee4c2e04bf014472c63fc3273d76d614a4
Joshua Snider <jsnider@mtu.edu>
2016-06-01 21:53:10 -0400

    Wrong file test operators..

--- testy.sh
+++ testy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ## This is the primary testy
-if [ -z /tmp/testy.tmp ]; then
+if [ ! -f /tmp/testy.tmp ]; then
   touch /tmp/testy.tmp && echo "I touched the tmp testy"
 else
   echo "I ain't touching it again.."
