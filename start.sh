6c11007ccecbfe7c9aa97862cfc15ff18c894eb8
Joshua Snider <jsnider@mtu.edu>
2021-11-09 09:33:16 -0500

    start.sh

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export MISP_URL=https://$(ip a show dev enp0s3|grep 'inet '|awk '{print $2}'|cut -d/ -f1)
+export MISP_AUTH_KEY=$(awk '{print $2}' /home/misp/MISP-authkey.txt)
+
+if [[ -e /home/misp/XFORCE-authkey.txt ]]; then
+  export XFORCE_API_KEY=$(awk '{print $2}' /home/misp/XFORCE-authkey.txt|head -1)
+  export XFORCE_API_PASS=$(awk '{print $2}' /home/misp/XFORCE-authkey.txt|tail -1)
+else
+  read -p "XForce API key: " XFORCE_API_KEY
+  export XFORCE_API_KEY=$XFORCE_API_KEY
+  echo "Apikey: $XFORCE_API_KEY" > /home/misp/XFORCE-authkey.txt
+  read -p "XForce API password: " XFORCE_API_PASS
+  export XFORCE_API_PASS=$XFORCE_API_PASS
+  echo "Apipass: $XFORCE_API_PASS" >> /home/misp/XFORCE-authkey.txt
+fi
+
+/home/misp/scripts/xforce.py &
